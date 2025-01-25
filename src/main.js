00be99280705c01073a440813ca436293496dfb7
Bogdan Denisenko <denisenkob15@gmail.com>
2025-01-25 19:47:12 +0300

    1.8 Добавит презентер путешествия

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
-import TripView from './view/trip-view.js';
 import FilterView from './view/filter-view.js';
-import SortView from './view/sort-view.js';
-import EventListView from './view/event-list-view.js';
-import EventView from './view/event-view.js';
-import EventAddView from './view/event-add-view.js';
-import EventEditView from './view/event-edit-view.js';
-// import EventListEmptyView from './view/event-list-empty-view.js';
-// import EventListLoadingView from './view/event-list-loading-view.js';
+import TripPresenter from './presenter/trip-presenter.js';
 import { render } from './render.js';
 
 const pageHeaderElement = document.querySelector('.page-header');
@@ -14,21 +7,8 @@ const filtersElement = pageHeaderElement.querySelector('.trip-controls__filters'
 const pageMainElement = document.querySelector('.page-body__page-main');
 const pageBodyContainerElement = pageMainElement.querySelector('.page-body__container');
 
+const tripPresenter = new TripPresenter({tripContainer: pageBodyContainerElement});
 
 render(new FilterView(), filtersElement);
-render(new TripView(), pageBodyContainerElement);
 
-const tripEventsElement = pageBodyContainerElement.querySelector('.trip-events');
-
-render(new SortView(), tripEventsElement);
-render(new EventListView(), tripEventsElement);
-
-const tripEventsListElement = pageBodyContainerElement.querySelector('.trip-events__list');
-
-render(new EventEditView(), tripEventsListElement);
-render(new EventAddView(), tripEventsListElement);
-
-for (let i = 0; i < 3; i++) {
-  render(new EventView(), tripEventsListElement);
-}
-
+tripPresenter.init();
