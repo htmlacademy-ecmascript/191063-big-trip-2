fcd16b1dc987e2edc154fe88f9a43e87e596753e
Bogdan Denisenko <denisenkob15@gmail.com>
2025-01-27 16:03:10 +0300

    Добавит вспомогательные функции для работы с датами

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
+import dayjs from 'dayjs';
+
+const MINUTES_IN_HOUR = 60;
+const HOURS_IN_DAY = 24;
+const MINUTES_IN_DAY = MINUTES_IN_HOUR * HOURS_IN_DAY;
+
 function getRandomInteger(max) {
   return Math.floor(Math.random() * max);
 }
@@ -6,4 +12,34 @@ function getRandomArrayElement(array) {
   return array[getRandomInteger(array.length)];
 }
 
-export {getRandomInteger, getRandomArrayElement};
+function getDay(date) {
+  return dayjs(date).format('MMM DD');
+}
+
+function getTime(date) {
+  return dayjs(date).format('HH:mm');
+}
+
+function getEventDuration(earlierDate, laterDate) {
+  const totalDifferenceInMinutes = dayjs(laterDate).diff(dayjs(earlierDate), 'minute');
+
+  const differenceInMinutes = `${(totalDifferenceInMinutes % MINUTES_IN_HOUR).toString().padStart(2, '0')}M`;
+  const differenceInHours = `${((Math.floor(totalDifferenceInMinutes / MINUTES_IN_HOUR)) % HOURS_IN_DAY).toString().padStart(2, '0')}H`;
+  const differenceInDays = `${(Math.floor(totalDifferenceInMinutes / MINUTES_IN_DAY)).toString().padStart(2, '0')}D`;
+
+  if (totalDifferenceInMinutes < MINUTES_IN_HOUR) {
+    return differenceInMinutes;
+  } else if (totalDifferenceInMinutes < MINUTES_IN_DAY) {
+    return `${differenceInHours} ${differenceInMinutes}`;
+  } else {
+    return `${differenceInDays} ${differenceInHours} ${differenceInMinutes}`;
+  }
+}
+
+export {
+  getRandomInteger,
+  getRandomArrayElement,
+  getDay,
+  getTime,
+  getEventDuration
+};
