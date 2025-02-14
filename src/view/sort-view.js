16493ea8e951bf342d66262d1cea674e9f7cad9c
Bogdan Denisenko <denisenkob15@gmail.com>
2025-02-14 18:36:10 +0300

    Поправит работу sort-view

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -5,7 +5,7 @@ function createSortTemplate() {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       <div class="trip-sort__item  trip-sort__item--day">
-        <input data-sort-type="${SortType.DAY}" id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
+        <input data-sort-type="${SortType.DAY}" id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
         <label class="trip-sort__btn" for="sort-day">Day</label>
       </div>
 
@@ -20,7 +20,7 @@ function createSortTemplate() {
       </div>
 
       <div class="trip-sort__item  trip-sort__item--price">
-        <input data-sort-type="${SortType.PRICE}" id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" checked>
+        <input data-sort-type="${SortType.PRICE}" id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
         <label class="trip-sort__btn" for="sort-price">Price</label>
       </div>
 
@@ -51,7 +51,6 @@ export default class SortView extends AbstractView {
       return;
     }
 
-    evt.preventDefault();
     this.#handleSortTypeChange(evt.target.dataset.sortType);
   };
 }
